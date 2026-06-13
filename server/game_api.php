cf19ebf11445872f294f0601ce413af7c091e0e5
MOUNBEKET NGOUNDAM V ABDEL FADIL <ngoundamroivelfadil@gmail.com>
2026-06-13 15:01:26 +0200

    premier commit

--- server/game_api.php
+++ server/game_api.php
@@ -98,8 +98,7 @@ try {
     http_response_code(500);
     echo json_encode([
         'success' => false, 
-        'error' => $e->getMessage(),
-        'debug' => 'Erreur API'
+        'error' => $e->getMessage()
     ]);
 }
 ?>
